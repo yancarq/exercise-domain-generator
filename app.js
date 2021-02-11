f4cf5532210da05ee43798cc0d5af98bb98240e0
yancarq <yancarlosrq@gmail.com>
2021-02-11 08:45:36 +0000

    Se agregan comentarios a las lineas

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var domain = ['com', 'net', 'us', 'io', 'cr'];
 GenerarUrlWeb();
 
 function GenerarUrlWeb() {
-    let urlGenerados = new Array()
+    let urlGenerados = new Array() //se recorren los arrays uno dentro de otro para lograr la distribucion con todos 
     for (let i = 0; i < pronoun.length; i++) {
         for (let j = 0; j < adj.length; j++) {
             for (let z = 0; z < noun.length; z++) {
@@ -25,12 +25,12 @@ function HackDomain(value,domain) {
 
     for (var i = 0; i < domain.length; i++) {
         let check = domain[i]
-        let search = new RegExp(check)
+        let search = new RegExp(check)  // se va colocando una expresion regular nueva del array de domain
      //   console.log(search)
-        if(search.test(value)){
-            value = value.replace(search, '');
+        if(search.test(value)){ // se valida si la expresion regilar esta contenidad en el noun para eliminarlo
+            value = value.replace(search, ''); // se procede a remplazar la concidencia de domain por nada
         }
         
     }
-    return value;
+    return value; //retorna el noun  sin la coincidencia del domain
 }
